d1ba590a4d41b278de5e12c150f1df5bc36e4d21
evprice <evprice99@gmail.com>
2022-01-08 12:00:28 -0800

    add template

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
+
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def homepage():
-    return "Evan", 200
+    return render_template("pw_page.html")
+
+
+@app.route("/generate_pw", methods = ['POST'])
+def create_pw():
+    print(request.form)
+
+    return "Working"
